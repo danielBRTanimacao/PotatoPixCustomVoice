d920e354bdbc84510f80fc7f597c8777f0dcb66b
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-12-01 15:20:02 -0300

    feat: add requirements creting url and init model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class CustomVoiceModel(models.Model):
+    name = models.CharField(max_length=100)
+    # ogg audio
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    '''
+    /api/voices?webhooktoken=123
+    backend_java_token=321
+    {
+        'voice': 'Bob esponja',
+        'msg': "Fulano mandou 10 conto. Bom dia como vai!"
+    }
+
+    PYTHON_WEBHOOK_KEY=123
+    PYTHON_BACKEND=321
+    '''
--- api/views.py
+++ api/views.py
@@ -1,8 +1,21 @@
+import os
+
 from ninja import NinjaAPI
 from ninja.responses import Response
+from ninja.errors import HttpError
+
+from dotenv import load_dotenv
 
 api = NinjaAPI()
+load_dotenv()
 
-@api.get('')
-def get_all_voices(request):
-    return Response({'data': 'data'})
+WEBHOOK_JAVA_SECRET = os.getenv('WEBHOOK_JAVA_SECRET')
+BACKEND_SECRET = os.getenv('BACKEND_SECRET') # passar o HEader depois
+
+@api.get('voices')
+def submit_voice(request, websecret: str):
+    if WEBHOOK_JAVA_SECRET != websecret:
+        print(WEBHOOK_JAVA_SECRET)
+        raise HttpError(401, "unauthorized websecret is invalid")
+    
+    return Response({'data': websecret})
