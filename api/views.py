03ffb97c1fd99d0fae90e5c3e2577d4e28afc397
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-12-02 19:57:15 -0300

    feat: updated views obj

--- api/views.py
+++ api/views.py
@@ -20,13 +20,10 @@ def submit_voice(request):
     if WEBHOOK_JAVA_SECRET != request.GET.get('websecret') or BACKEND_SECRET != request.headers.get('x-backend-secret'):
         return Response("unauthorized websecret or header is invalid", status=status.HTTP_401_UNAUTHORIZED)
 
-    name_to_find = request.POST.get('name')
+    name_to_find = request.data.get('name')
     if not name_to_find:
         return Response({"detail": "field 'name' is required."}, status=status.HTTP_400_BAD_REQUEST)
     
-    try:
-        voice_obj = get_object_or_404(CustomVoiceModel, name__icontains=name_to_find)
-    except ValueError as e:
-        return Response({"detail": f"Err to find obj: {e}"}, status=status.HTTP_400_BAD_REQUEST)
+    voice_obj = get_object_or_404(CustomVoiceModel, name__icontains=name_to_find)
     
     return Response()
