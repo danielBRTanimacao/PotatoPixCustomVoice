4f0525530bbe24d25185a1705411cc1ec53bbda4
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-12-03 15:38:09 -0300

    feat: updating generation

--- utils/generate_voice.py
+++ utils/generate_voice.py
@@ -1,14 +1,25 @@
-import torch
-
-x = torch.rand(5, 3)
-print(x)
-
-print(torch.cuda.is_available()) # CUDA ta desativado mas acho que com CPU da pra clonar a voz
-
-# ========== ACho que vai ser necessario ==============
-# Usar o tts Text to speach para transfomrar o texto em audio torch combinado para machine learning
+"""
 # usar uma voz de base (obrigatorio)
 # ter uma mensagem (obrigatorio)
 # uma linguagem tipo (pt, en, es e etc)
 # se vai ser CPU ou GPU para rodar a aplicação
 # salvar o model com o audio gerado e a prerenderização para testes
+"""
+
+from pathlib import Path
+
+import torch
+import pyttsx3
+from tempfile import NamedTemporaryFile
+
+BASE_DIR = Path(__file__).resolve().parent
+
+VOICE_DIR = BASE_DIR / 'voices'
+MSG = "Iae meu nobre, como vai?"
+
+engine = pyttsx3.init()
+
+with NamedTemporaryFile(dir=VOICE_DIR, suffix='.mp3') as voice:
+    # gerar a voz
+    # salvar
+    ...
