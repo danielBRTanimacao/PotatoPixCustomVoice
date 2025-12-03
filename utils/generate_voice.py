d9b13552d6710ed2a878afc2c9cc7ee23b735c65
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-12-03 20:56:59 -0300

    feat: changing some ports

--- utils/generate_voice.py
+++ utils/generate_voice.py
@@ -1,25 +1,37 @@
-"""
-# usar uma voz de base (obrigatorio)
-# ter uma mensagem (obrigatorio)
-# uma linguagem tipo (pt, en, es e etc)
-# se vai ser CPU ou GPU para rodar a aplicação
-# salvar o model com o audio gerado e a prerenderização para testes
-"""
-
 from pathlib import Path
 
 import torch
-import pyttsx3
+from TTS.api import TTS
 from tempfile import NamedTemporaryFile
+import numpy
+import io
+import soundfile
 
 BASE_DIR = Path(__file__).resolve().parent
 
 VOICE_DIR = BASE_DIR / 'voices'
+VOICE_FILE = VOICE_DIR / 'bobEsponja.wav'
+LANG = 'pt'
 MSG = "Iae meu nobre, como vai?"
+DEVICE = 'cpu'
 
-engine = pyttsx3.init()
+if torch.cuda.is_available():
+    DEVICE = 'cuda'
+
+tts = TTS('tts_models/multilingual/multi-dataset/xtts_v2').to(DEVICE)
 
 with NamedTemporaryFile(dir=VOICE_DIR, suffix='.mp3') as voice:
-    # gerar a voz
-    # salvar
-    ...
+    voice.write(VOICE_FILE.file.read())
+    voice_path = voice.name
+    
+    speech_file = tts.tts(
+        text=MSG,
+        speaker_mp3=voice_path,
+        language=LANG
+    )
+
+audio_array = numpy.array(speech_file, dtype=numpy.float32)
+
+audio_stream= io.BytesIO()
+soundfile.write(audio_stream, audio_array, samplerate=22050, format='mp3')
+audio_stream.seek(0)
