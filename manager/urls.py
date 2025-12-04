32db29a4e6969c253c2acc9e74f6243fc6b5afe3
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-12-04 17:18:47 -0300

    feat: testing new url making a get

--- manager/urls.py
+++ manager/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('voices/webhook', views.submit_voice),
+    path('voices', views.list_all_voices),
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 @api_view(['GET'])
 def list_all_voices(request):
-    ...
+    return Response({'data': 'data'})
 
 # create
