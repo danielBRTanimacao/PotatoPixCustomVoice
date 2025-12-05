4dd3504f277b5e568eb44005fe9c01a0b6c2f5c7
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-12-05 18:06:09 -0300

    feat: basic CRUD for manager

--- manager/views.py
+++ manager/views.py
@@ -20,3 +20,15 @@ class ManagerCustomVoice(APIView):
             voice_serializer.save()
             return Response(status.HTTP_201_CREATED)
         return Response(voice_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        voice_obj = CustomVoiceModel.objects.get(pk=pk)
+        voice_obj_serializer = InCustomVoiceSerializer(voice_obj, data=request.data)
+        if voice_obj_serializer.is_valid(raise_exception=True):
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(voice_obj_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+     
+    def delete(self, pk: int, request):
+        voice_obj = CustomVoiceModel.objects.get(pk=pk)
+        voice_obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
