8e2e8165d3fa9f33afae5a46358bdd97645d9722
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-12-09 09:07:16 -0300

    feat: using celery

--- core/settings.py
+++ core/settings.py
@@ -31,9 +31,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'core.urls'
 
